b13d332cfad1cbf34a8e40c9435a2a2b7104d4ae
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-04-09 12:51:00 +0930

    set up nodemon

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow | null;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    title: "Adelaide Metro",
+    frame: true,
+    autoHideMenuBar: true,
+    resizable: true,
+    darkTheme: true,
+  });
+
+  mainWindow.loadFile('src/index.html');
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+console.log("teste")
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
