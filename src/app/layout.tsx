8191b88ee9cc28b497dd08e16f8dc6745c24eb42
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-20 14:26:13 +0930

    make google map embed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+      </head>
       <body className={inter.className}>
         <Header />
         <div className="content">
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,6 +5,14 @@ export default function History() {
     <>
       <div className="">
         <h1>Map</h1>
+
+        <iframe
+          width="600"
+          height="450"
+          loading="lazy"
+          src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+            &q=Space+Needle,Seattle+WA">
+        </iframe>
       </div>  
     </>
   );
