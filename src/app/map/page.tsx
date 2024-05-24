2b7b8cb2a557df44f2a0b3dc62bba93c3ad467f0
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-24 17:20:33 +0930

    Add map input boxes

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,17 +1,57 @@
 'use client';
 
+import { useRef, useState, MutableRefObject} from 'react';
 import styles from './styles.module.scss'
 
-export default function History() {
+// AIzaSyAWifE8HCuECIIVcvpArqw-4qfdz3rZDrw
+
+export default function Map() {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+
+  let toInput: MutableRefObject<HTMLElement | undefined> = useRef(undefined);
+  let fromInput: MutableRefObject<HTMLElement | undefined> = useRef(undefined);
+
+  function buttonPressed() {
+    if (toInput !== undefined && fromInput !== undefined) {
+      let fromText = fromInput.current?.textContent?.toLowerCase();
+      fromText?.replaceAll(" ", "+");
+
+      let toText = toInput.current?.textContent?.toLowerCase();
+      toText?.replaceAll(" ", "+");
+
+      setFrom(fromText || "")
+      setTo(toText || "")
+    } 
+  }
+
+  function addMap() {
+    if (to === "" || from === "") {
+      return;
+    }
+
+    let apiKey = "AIzaSyAWifE8HCuECIIVcvpArqw-4qfdz3rZDrw"
+
+    let url = `https://www.google.com/maps/embed/v1/directions?key=${apiKey}&mode=transit&origin=${from}&destination=${to}`
+
+    return (
+      <iframe
+          loading="lazy"
+          src={url}>
+      </iframe>
+    )
+  }
+
   return (
     <div className="">
       <h1>Map</h1>
 
       <div className={styles.primary}>
-        <iframe
-          loading="lazy"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAWifE8HCuECIIVcvpArqw-4qfdz3rZDrw&q=ADELAIDE+SA">
-        </iframe>
+        <input type="text" placeholder='From' ref={fromInput}/>
+        <input type="text" placeholder='To' ref={toInput}/>
+        <button onClick={buttonPressed}>Go!</button>
+
+        {addMap()}
       </div>
     </div>  
   );
