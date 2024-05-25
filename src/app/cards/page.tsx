e91c15076ff11244fe9c60bbc28d08f6398f2027
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-25 16:06:58 +0930

    Start cards

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import Card from "@/components/card";
+
 export default function Cards() {
   return (
     <>
       <div className="">
         <h1>Cards</h1>
+        <Card type='regular' />
       </div>  
     </>
   );
---
+++ src/components/card/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import styles from './styles.module.scss'
+
+export default function Card({ type }: { type: 'regular' | 'student' | 'concesion' }) {
+    return (
+        <div className={styles[type]}>
+            <h2>Balance</h2>
+            <h1>$5.00</h1>
+            <h2>{type}</h2>    
+        </div>
+    )
+}
