4807029d09dbe1ecb5e3a386d9a534eabf9b3ed4
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-27 14:15:09 +0930

    Work on cards

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -5,10 +5,12 @@ import Card from "@/components/card";
 export default function Cards() {
   return (
     <>
-      <div className="">
-        <h1>Cards</h1>
-        <Card type='regular' />
-      </div>  
+      <h1>Cards</h1>
+      <Card type='regular' />
+      <Card type='regular' />
+      <Card type='student' />
+      <Card type='concession' />
+      <Card type='student' /> 
     </>
   );
 }
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,7 +2,7 @@
 
 import styles from './styles.module.scss'
 
-export default function Card({ type }: { type: 'regular' | 'student' | 'concesion' }) {
+export default function Card({ type }: { type: 'regular' | 'student' | 'concession' }) {
     return (
         <div className={styles[type]}>
             <h2>Balance</h2>
