14e360c4f58ea14aea28cd600220f15c01c0aa24
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-28 12:48:25 +0930

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true
-    },
- }
+  basepath: "/metro-app",
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
