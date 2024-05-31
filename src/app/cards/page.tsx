682b38da50e5c558b04692638eb93cd899b424bc
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-05-31 10:49:09 +0930

    Update layout

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -2,15 +2,26 @@
 
 import Card from "@/components/card";
 
+import { Plus } from "lucide-react";
+
+import styles from './styles.module.scss'
+
 export default function Cards() {
   return (
-    <>
+    <div className={styles.primary}>
       <h1>Cards</h1>
-      <Card type='regular' />
-      <Card type='regular' />
-      <Card type='student' />
-      <Card type='concession' />
-      <Card type='student' /> 
-    </>
+
+      <div className={styles.list}>
+        <Card type='regular' />
+        <Card type='regular' />
+        <Card type='student' />
+        <Card type='concession' />
+        <Card type='student' /> 
+
+        <div className={styles.new}>
+          <Plus id="large"/>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,9 +5,14 @@ import styles from './styles.module.scss'
 export default function Card({ type }: { type: 'regular' | 'student' | 'concession' }) {
     return (
         <div className={styles[type]}>
-            <h2>Balance</h2>
-            <h1>$5.00</h1>
-            <h2>{type}</h2>    
+            <div className={styles.balance}>
+                <h2>Balance</h2>
+                <h1>$5.00</h1>
+            </div>
+
+            <div className={styles.type}>
+                <h2>{type[0].toUpperCase() + type.slice(1)}</h2>    
+            </div>
         </div>
     )
 }
