a33f48bb56a1df28e57a93570c92613b41de2c85
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-06-03 23:00:45 +0930

    Fix rerender issue

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,13 +5,32 @@ import styles from './styles.module.scss'
 import { CircleCheck } from 'lucide-react';
 import { Settings } from 'lucide-react';
 
+import { useState } from 'react';
+
 export default function Header() {
+    // const [minutes, setMinutes] = useState(52);
+    // const [seconds, setSeconds] = useState(0);
+
+    // if (seconds === 0) {
+    //     setMinutes(minutes - 1);
+    // }
+
+    // if (minutes === 0) {
+    //     setMinutes(59);
+    // }
+
+    // setInterval(() => {
+    //     if (seconds - 1 === 0) {
+    //         setSeconds(seconds - 1);
+    //     }
+    // }, 1)
+    
     return (
         <div className={styles.primary}>
             <div className="empty" />
             <div className={styles.validated}>
                 <CircleCheck id="small" />
-                <h3>52:00</h3>
+                <h3>`59:00`</h3>
             </div>
 
             <div className={styles.settings}>
