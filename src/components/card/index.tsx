47fe739e4fe0f62bcfa4bcf1459bd008b9e42ee1
Kadison <134670047+KadisonM@users.noreply.github.com>
2024-06-03 13:51:47 +0930

    Update card styling

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,14 +5,18 @@ import styles from './styles.module.scss'
 export default function Card({ type }: { type: 'regular' | 'student' | 'concession' }) {
     return (
         <div className={styles[type]}>
-            <div className={styles.balance}>
-                <h2>Balance</h2>
-                <h1>$5.00</h1>
-            </div>
+            <div className={styles.left}>
+                <div className=""></div>
+            
+                <div className={styles.balance}>
+                    <h2>Balance</h2>
+                    <h1>$5.00</h1>
+                </div>
 
-            <div className={styles.type}>
-                <h2>{type[0].toUpperCase() + type.slice(1)}</h2>    
-            </div>
+                <div className={styles.type}>
+                    <h2>{type[0].toUpperCase() + type.slice(1)}</h2>    
+                </div>
+            </div>     
         </div>
     )
 }
